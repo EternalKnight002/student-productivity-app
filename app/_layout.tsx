680759ceba8a13aefe338a7feabe9dd0cd05106e
Aman <2301641550018@aktuacin.onmicrosoft.com>
2025-10-27 15:45:38 +0530

    upto header done

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,21 +1,22 @@
-
 import 'react-native-get-random-values';
-
 import React from 'react';
 import { Stack } from 'expo-router';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { View } from 'react-native';
 
-
 export default function Layout() {
-return (
-<GestureHandlerRootView style={{ flex: 1 }}>
-<SafeAreaProvider style={{ flex: 1 }}>
-<Stack>
-<Stack.Screen name="index" options={{ headerShown: false }} />
-</Stack>
-</SafeAreaProvider>
-</GestureHandlerRootView>
-);
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaProvider style={{ flex: 1 }}>
+        <Stack
+          screenOptions={{
+            headerShown: false, 
+          }}
+        >
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+        </Stack>
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
+  );
 }
